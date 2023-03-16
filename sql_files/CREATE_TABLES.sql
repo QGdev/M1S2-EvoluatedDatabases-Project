3968a40b199cc34cc8977d3dee2839ed507ba33e
Matthéo <mattheo9912@protonmail.com>
2023-03-16 15:20:34 +0100

    Fixing minor error in CREATE TABLES sql file

--- sql_files/CREATE_TABLES.sql
+++ sql_files/CREATE_TABLES.sql
@@ -18,31 +18,31 @@ CREATE TABLE COUNTRIES (
     id_country VARCHAR(3) NOT NULL,
     country_name VARCHAR(48),
     CONSTRAINT pk_country PRIMARY KEY (id_country),
-    CONSTRAINT id_country_format CHECK (REGEXP_LIKE(id_country, '^[A-Z]{3}$'))
+    CONSTRAINT id_country_format CHECK(REGEXP_LIKE(id_country, '^[A-Z]{3}$'))
 );
 
 --
 --  Creation de la dimension AGE_GROUP
 --
 CREATE TABLE AGE_GROUPS (
-    id_age_group VARCHAR(2) NOT NULL,
+    id_age_group INTEGER NOT NULL,
     age_group VARCHAR(7),
     CONSTRAINT pk_age_group PRIMARY KEY (id_age_group),
-    CONSTRAINT id_age_group_format CHECK (REGEXP_LIKE(id_age_group, '^[A-Z]*$')),
-    CONSTRAINT age_group_format CHECK (REGEXP_LIKE(id_age_group, '^[0-9A-Z_ -]*$'))
+    CONSTRAINT id_age_group_format CHECK(id_age_group >= 0),
+    CONSTRAINT age_group_format CHECK(REGEXP_LIKE(age_group, '^(([0-9]{1,2} - [0-9]{1,2})|(LESS_[0-9]{1,2})|([0-9]{1,2}_OVER)|TOTAL)$'))
 );
 
 --
 --  Creation de la table des faits
 --
 CREATE TABLE FACTS (
-    id_country VARCHAR(3) NOT NULL CHECK (REGEXP_LIKE(id_country, '^[A-Z]{3}$')),
+    id_country VARCHAR(3) NOT NULL CHECK(REGEXP_LIKE(id_country, '^[A-Z]{3}$')),
     year NUMBER(4) NOT NULL CHECK (year >= 1 AND year <= 9999),
     hdi FLOAT CHECK (hdi >= 0 AND hdi <= 1),
     gni FLOAT CHECK (gni >= 0),
     co2 FLOAT CHECK (co2 >= 0),
-    id_age_group VARCHAR(2) NOT NULL CHECK (REGEXP_LIKE(id_age_group, '^[A-Z]*$')),
-    population_count NUMBER CHECK (population_count >= 0),
+    id_age_group INTEGER NOT NULL CHECK(id_age_group >= 0),
+    population_count NUMBER CHECK(population_count >= 0),
     CONSTRAINT pk_facts PRIMARY KEY (id_country, id_age_group, year),
     CONSTRAINT fk_facts_country FOREIGN KEY (id_country) REFERENCES COUNTRIES(id_country) ON DELETE CASCADE,
     CONSTRAINT fk_facts_age_group FOREIGN KEY (id_age_group) REFERENCES AGE_GROUPS(id_age_group) ON DELETE CASCADE
