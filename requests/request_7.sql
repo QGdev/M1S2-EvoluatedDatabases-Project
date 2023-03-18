9a25c237d6457c7ee9e55d3a84416c39bc478363
Quentin GOMES DOS REIS <quentin.gomes-dos-reis@etu.univ-nantes.fr>
2023-03-19 00:54:59 +0100

    Modfification of request number 7

--- requests/request_7.sql
+++ requests/request_7.sql
@@ -11,6 +11,6 @@
 
 SELECT p.country_name, a.age_group, AVG(f.population_count) AS MoyPopulation, DENSE_RANK() OVER (PARTITION BY a.age_group ORDER BY(AVG(f.population_count)) DESC) AS rank
 FROM FACTS f, COUNTRIES p, AGE_GROUPS a
-WHERE p.id_country = f.id_country AND f.id_age_group = a.id_age_group AND f.population_count IS NOT NULL
+WHERE p.id_country = f.id_country AND f.id_age_group = a.id_age_group AND f.population_count IS NOT NULL AND a.age_group <> 'TOTAL'
 GROUP BY (p.country_name, a.age_group)
 ORDER BY rank;
