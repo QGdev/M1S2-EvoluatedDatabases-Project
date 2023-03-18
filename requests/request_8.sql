dba967af0f180ddd59a559b2b0075bb02ce6c94e
Quentin GOMES DOS REIS <quentin.gomes-dos-reis@etu.univ-nantes.fr>
2023-03-18 22:54:57 +0100

    Correction of sort order in request 8

--- requests/request_8.sql
+++ requests/request_8.sql
@@ -14,5 +14,5 @@ FROM (SELECT p.country_name, SUM(f.population_count * f.co2) AS emission
 FROM FACTS f, COUNTRIES p, AGE_GROUPS a
 WHERE f.id_country = p.id_country AND f.id_age_group = a.id_age_group AND f.year=2021 AND a.age_group='TOTAL' AND f.population_count IS NOT NULL AND f.co2 IS NOT NULL
 GROUP BY p.country_name
-ORDER BY emission)
+ORDER BY emission DESC)
 WHERE ROWNUM <= 10;
