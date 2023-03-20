41063dee7c499a551d30403203e362d87922c9da
Quentin GOMES DOS REIS <quentin.gomes-dos-reis@etu.univ-nantes.fr>
2023-03-20 20:46:29 +0100

    Adjusting request 1 and push the result

--- requests/request_1.sql
+++ requests/request_1.sql
@@ -10,6 +10,6 @@
 --Les émissions de co2 par pays et par année
 
 SELECT p.country_name, f.year, SUM(f.co2)
-FROM COUNTRIES p, FACTS f
-WHERE p.id_country = f.id_country AND f.co2 IS NOT NULL
+FROM COUNTRIES p, FACTS f, AGE_GROUPS a
+WHERE p.id_country = f.id_country AND f.id_age_group = a.id_age_group AND a.age_group = 'TOTAL' AND f.co2 IS NOT NULL
 GROUP BY CUBE(p.country_name,f.year);
